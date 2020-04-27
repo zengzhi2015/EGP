d8cf5b91b368f39b5c6e0bbfa669dc27b13c3d2e
Zhi Zeng <zengzhi_2012@hotmail.com>
2020-04-27 11:57:48 +0800

    Add files via upload

---
+++ non_zeros_max_step_guarrentee_3D.m
@@ -0,0 +1,19 @@
+%% Modify the xPhys to solve the split-stepping numerical problem
+function new_xPhys=non_zeros_max_step_guarrentee_3D(xPhys,volfrac,Emin)
+    gap_min = 0.2; % This should not be smaller than the fraction
+    if gap_min>=volfrac
+        gap_min = volfrac*0.75;
+    end
+    new_xPhys = xPhys;
+    new_xPhys(xPhys<Emin+gap_min)=Emin;
+    new_xPhys(xPhys>1.0-gap_min)=1.0;
+    if length(size(xPhys)) == 2
+        [nelx,nely] = size(xPhys);
+        vol_diff = sum(new_xPhys,'all')-volfrac*nelx*nely;
+    else
+        [nely,nelx,nelz] = size(xPhys);
+        vol_diff = sum(new_xPhys,'all')-volfrac*nelx*nely*nelz;
+    end
+    changable_flag = (new_xPhys>=Emin+gap_min) & (new_xPhys<=1.0-gap_min);
+    new_xPhys(changable_flag) = xPhys(changable_flag)-vol_diff/sum(changable_flag,'all');
+end
